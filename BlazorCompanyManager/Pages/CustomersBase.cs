2bb09f1a9af632f961453c9028e8ce7895fa28d2
vadimffe <vadim.ffegmail.com>
2021-11-20 14:08:02 +0200

    Fixed Dialog dragging and textbox issue

--- BlazorCompanyManager/Pages/CustomersBase.cs
+++ BlazorCompanyManager/Pages/CustomersBase.cs
@@ -1,9 +1,11 @@
 ﻿using BlazorCompanyManager.Data;
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +18,8 @@ namespace BlazorCompanyManager.Pages
 
     [Inject]
     protected IRepository Repository { get; set; }
+
+    private readonly IJSRuntime JSRuntime;
 
     protected BlazorCompanyManager.Data.Customer customer { get; set; }
     protected List<BlazorCompanyManager.Data.Customer> customers;
@@ -100,6 +104,40 @@ namespace BlazorCompanyManager.Pages
       }
 
       this.customers = await this.Repository.GetCustomersAsync();
+    }
+
+    public async Task DownloadExcelDocument()
+    {
+      using (var workbook = new XLWorkbook())
+      {
+        IXLWorksheet worksheet =
+        workbook.Worksheets.Add("Customers");
+        worksheet.Cell(1, 1).Value = "Company name";
+        worksheet.Cell(1, 2).Value = "City";
+
+        for (int i = 1; i <= 2; i++)
+        {
+          worksheet.Cell(1, i).Style.Font.Bold = true;
+        }
+
+        int index = 1;
+        foreach (var customer in this.customers)
+        {
+          worksheet.Cell(index + 1, 1).Value = customer.CompanyName;
+          worksheet.Cell(index + 1, 2).Value = customer.City;
+
+          index++;
+        }
+
+        using (var stream = new MemoryStream())
+        {
+          workbook.SaveAs(stream);
+          var content = stream.ToArray();
+
+          var fileName = "Customers.xlsx";
+          await JSRuntime.InvokeAsync<object>("saveAsFile", fileName, Convert.ToBase64String(content));
+        }
+      }
     }
   }
 }
