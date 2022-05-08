b015b11b0d5d1b95e630385f298132b84758d7f2
vadimffe <vadim.ffe@gmail.com>
2022-05-08 21:35:21 +0300

    Code cleaned, added possibility to download Customer list in Excel file format

--- BlazorCompanyManager/Pages/Customers.razor.cs
+++ BlazorCompanyManager/Pages/Customers.razor.cs
@@ -11,15 +11,13 @@ using System.Threading.Tasks;
 
 namespace BlazorCompanyManager.Pages
 {
-  public class CustomersBase : ComponentBase
+  public partial class Customers : ComponentBase
   {
     [Parameter]
     public string Id { get; set; }
 
     [Inject]
     protected IRepository Repository { get; set; }
-
-    private readonly IJSRuntime JSRuntime;
 
     protected BlazorCompanyManager.Data.Customer customer { get; set; }
     protected List<BlazorCompanyManager.Data.Customer> customers;
@@ -30,7 +28,7 @@ namespace BlazorCompanyManager.Pages
     protected bool isLoaded = false;
     bool isHovering = false;
 
-    public CustomersBase()
+    public Customers()
     {
       this.customer = new Data.Customer();
       this.customers = new List<Customer>();
@@ -127,6 +125,57 @@ namespace BlazorCompanyManager.Pages
     public List<Customer> GetTable()
     {
       return this.customers;
+    }
+
+    private async Task DownloadFile()
+    {
+      using (var workbook = new XLWorkbook())
+      {
+        IXLWorksheet worksheet =
+        workbook.Worksheets.Add("Customers");
+        worksheet.Cell(1, 1).Value = "Company name";
+        worksheet.Cell(1, 2).Value = "City";
+        worksheet.Cell(1, 3).Value = "Address";
+        worksheet.Cell(1, 4).Value = "Post code";
+        worksheet.Cell(1, 5).Value = "Phone number";
+        worksheet.Cell(1, 6).Value = "Business ID";
+
+        for (int i = 1; i <= 6; i++)
+        {
+          worksheet.Cell(1, i).Style.Font.Bold = true;
+        }
+
+        int index = 1;
+        foreach (var customer in this.customers)
+        {
+          worksheet.Cell(index + 1, 1).Value = customer.CompanyName;
+          worksheet.Cell(index + 1, 2).Value = customer.City;
+          worksheet.Cell(index + 1, 3).Value = customer.Address;
+          worksheet.Cell(index + 1, 4).Value = customer.PostCode;
+          worksheet.Cell(index + 1, 5).Value = customer.PhoneNumber;
+          worksheet.Cell(index + 1, 6).Value = customer.BusinessID;
+
+          index++;
+        }
+
+        worksheet.Columns().AdjustToContents();  // Adjust column width
+        worksheet.Rows().AdjustToContents();
+
+        var firstCell = worksheet.FirstCellUsed();
+        var lastCell = worksheet.LastCellUsed();
+        var range = worksheet.Range(firstCell.Address, lastCell.Address);
+
+        var table = range.CreateTable();
+
+        using (var stream = new MemoryStream())
+        {
+          workbook.SaveAs(stream);
+          var content = stream.ToArray();
+
+          var fileName = "Customers.xlsx";
+          await jsRuntime.InvokeAsync<object>("saveAsFile", fileName, Convert.ToBase64String(content));
+        }
+      }
     }
   }
 }
